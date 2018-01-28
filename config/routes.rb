8e7adc63de53908ffb7fb7a42a8b35140e6df946
ascalmon <ascalmon@gmail.com>
2018-01-27 20:09:48 -0500

    Alpha_blog working users, articles, signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
   resources :articles
   root 'pages#home'
   get 'about', to: 'pages#about'
-  
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
