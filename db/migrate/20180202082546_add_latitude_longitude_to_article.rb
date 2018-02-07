1c20a978fa78896c1c74dad72c176972d2be552b
ascalmon <ascalmon@gmail.com>
2018-02-07 18:34:37 -0200

    Teste

--- db/migrate/20180202082546_add_latitude_longitude_to_article.rb
+++
@@ -1,6 +0,0 @@
-class AddLatitudeLongitudeToArticle < ActiveRecord::Migration[5.1]
-  def change
-    add_column :articles, :latitude, :float
-    add_column :articles, :longitude, :float
-  end
-end
--- db/migrate/20180202082842_add_country_city_location_to_article.rb
+++
@@ -1,7 +0,0 @@
-class AddCountryCityLocationToArticle < ActiveRecord::Migration[5.1]
-  def change
-    add_column :articles, :country, :string
-    add_column :articles, :city, :string
-    add_column :articles, :location, :text
-  end
-end
